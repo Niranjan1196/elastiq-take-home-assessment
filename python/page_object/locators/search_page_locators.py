62420e559c6340d0287df1e779f439b9356e439d
Niranjan1196 <niranjansangale96@gmail.com>
2025-01-14 11:00:40 +0530

    Added respective page locator under 'search_page_locator.py' file.

---
+++ python/page_object/locators/search_page_locators.py
@@ -0,0 +1,6 @@
+
+class SearchPageLocators:
+    # Locators for Search Page
+    UI_ELEMENT_SEARCH_BOX = "//input[@type='search']"
+    UI_ELEMENT_TABLE_ROWS = "//table[@id='example']/tbody//tr"
+    UI_ELEMENT_PAGINATION_RECORDS = "//div[@class='dataTables_info']"
